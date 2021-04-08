fc4d5149d110bc8510b228630dad35caa6952b99
Ancacion <37138626+Ancacion@users.noreply.github.com>
2021-04-08 11:16:59 +0800

    0408 update

--- lab02/dataLoader.js
+++ lab02/dataLoader.js
@@ -3,7 +3,7 @@ var title = "treemap";
 
 function dataLoader(text,cb) {
 	//有些csv資料的seperator是使用分號，這邊統一使用逗號座分隔
-    var uri = 'data:text/plain;base64,' + Base64.encode(text.replace(/;/g, ','))
+    var uri = text//'data:text/plain;base64,' + Base64.encode(text.replace(/;/g, ','))
     var dsv = d3.dsvFormat(",")
 
     d3.csv(uri, function(rawData){
